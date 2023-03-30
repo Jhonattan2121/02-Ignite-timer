4b866985d925ed43a81f4c24f60cb47ddb528a9d
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-29 23:41:03 -0300

    Salvando estado no storage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -20,13 +20,12 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
           setSecondsPassed(totalSeconds)
-
           clearInterval(interval)
         } else {
           setSecondsPassed(secondsDifference)
